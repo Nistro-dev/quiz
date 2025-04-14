9f69e26dea58f6d2549e802bf998c5cf7e231058
Nistro-dev <micmael86@gmail.com>
2025-04-14 11:01:43 +0200

    feat: add routes for Question and QuizResponse with CRUD operations

--- backend/src/routes/quizRoutes.js
+++ backend/src/routes/quizRoutes.js
@@ -2,17 +2,29 @@ import express from "express";
 import {
   createQuiz,
   getQuizzes,
-  getQuizById,
-  updateQuiz,
   deleteQuiz,
+  getQuizById,
+  getQuizzesByTheme,
+  updateQuiz
 } from "../controllers/quizController.js";
+import { getQuestionsByQuiz } from "../controllers/questionController.js";
 
 const router = express.Router();
 
-router.post("/quiz", createQuiz);
-router.get("/quiz", getQuizzes);
-router.get("/quiz/:id", getQuizById);
-router.put("/quiz/:id", updateQuiz);
-router.delete("/quiz/:id", deleteQuiz);
+router
+  .route("/")
+  .post(createQuiz)
+  .get(getQuizzes);
+router
+  .route("/:id")
+  .get(getQuizById)
+  .delete(deleteQuiz)
+  .put(updateQuiz);
+router
+  .route("/theme/:theme")
+  .get(getQuizzesByTheme);
+router
+  .route("/:id/questions")
+  .get(getQuestionsByQuiz);
 
 export default router;
