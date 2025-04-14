caf53f966156764390ef6d006530eab9b878135f
Nistro-dev <micmael86@gmail.com>
2025-04-14 11:01:48 +0200

    feat: update API routes for quiz, quizResponse, and question with proper endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import quizRoutes from "./routes/quizRoutes.js";
+import quizResponseRoutes from "./routes/quizResponseRoutes.js";
+import questionRoutes from "./routes/questionRoutes.js";
+import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use("/api", quizRoutes);
+app.use("/api/quiz", quizRoutes);
+app.use("/api/quizResponse", quizResponseRoutes);
+app.use("/api/question", questionRoutes);
 
 export default app;
